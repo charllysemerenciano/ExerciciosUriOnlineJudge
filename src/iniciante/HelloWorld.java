4d66116cdee5d3b31d4f03f5347f049590ed5d81
Charllys Emerenciano <charllysemerenciano@gmail.com>
2020-11-13 22:57:50 -0300

    HelloWorld #1000

---
+++ src/iniciante/HelloWorld.java
@@ -0,0 +1,11 @@
+package iniciante;
+
+/**
+ * @Author Charllys Emerenciano
+ * @create 13/11/2020 22:55
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
