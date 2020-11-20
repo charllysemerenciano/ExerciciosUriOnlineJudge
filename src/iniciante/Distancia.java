816e10fa45c7ec36d0980b3da2d6a3668cef237c
Charllys Emerenciano <charllysemerenciano@gmail.com>
2020-11-19 23:03:41 -0300

    Distância #1016

---
+++ src/iniciante/Distancia.java
@@ -0,0 +1,20 @@
+package iniciante;
+
+import java.util.Scanner;
+
+/**
+ * @Author Charllys Emerenciano
+ * @create 19/11/2020 22:59
+ */
+public class Distancia {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int km = scanner.nextInt();
+
+        System.out.println(km * 2 + " minutos");
+
+
+    }
+}
